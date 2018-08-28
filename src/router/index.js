2ac51e863fa9fa04d5e4d30efc92a3a5a54c7f59
Marcelo Barbosa <marceloalbuquerquebarbosa@gmail.com>
2018-08-27 23:09:41 -0300

    cadastro router

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import Layout from '../components/Layout'
 import Login from '../pages/Login'
 import Menu from '../pages/Menu'
+import Cadastro from '../pages/Cadastro'
 
 Vue.use(Router)
 
@@ -21,6 +22,10 @@ const router = new Router({
           {
             path: '/menu',
             component: Menu
+          },
+          {
+            path: '/cadastro',
+            component: Cadastro
           }
         ]
       },
